4d868c08a9450864d20024ed21246e2972865a3b
Ihsaanul Hoque <ihsaanul.hoque@hotmail.com>
2020-10-28 05:12:21 +0000

    debugging exercide 1 complete

---
+++ 2 Software Engineering Foundations/4 Debugging/debugging_exercise_1/lib/letter_count.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Debug this code to pass rspec! There are 3 mistakes to fix.
+
+# Write a method, letter_count, that accepts a string and char as args.
+# The method should return the number of times that the char appears in the string.
+
+require 'byebug'
+
+def letter_count(string, char)
+  count = 0
+  string.downcase.each_char do |c|
+    count += 1 if c == char
+  end
+  count
+end
+
+p letter_count('apple', 'p')
+p letter_count('apple', 'q')
+p letter_count('MISSIPPI', 'i')
